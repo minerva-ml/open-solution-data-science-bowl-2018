efc2544ddc4cd7946e28e61be20f3d0655246a4f
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-02-08 16:24:04 +0100

    hand picked values for augmentations

--- augmentation.py
+++ augmentation.py
@@ -5,26 +5,30 @@ affine_seq = iaa.Sequential([
     iaa.SomeOf((1, 2),
                [iaa.Fliplr(0.5),
                 iaa.Flipud(0.5),
-                iaa.Affine(rotate=180),
-                iaa.CropAndPad(percent=(-0.5, 0.5), pad_cval=(0)),
+                iaa.Affine(rotate=(0, 360),
+                           translate_percent=(-0.0, 0.1)),
+                iaa.CropAndPad(percent=(-0.5, 0.5)),
                 ]),
 
     # Deformations
-    iaa.OneOf(
-        [iaa.Affine(shear=(-16, 16)),
-         iaa.PiecewiseAffine(scale=(0.01, 0.05))
-         ])
-
+    iaa.PiecewiseAffine(scale=(0.01, 0.05))
 ], random_order=True)
 
 color_seq = iaa.Sequential([
     # Color
-    iaa.OneOf(
-        [iaa.Sequential([
+    iaa.OneOf([
+        iaa.Sequential([
             iaa.ChangeColorspace(from_colorspace="RGB", to_colorspace="HSV"),
             iaa.WithChannels(0, iaa.Add((50, 100))),
-            iaa.ChangeColorspace(from_colorspace="HSV", to_colorspace="RGB")
-        ]),
-            iaa.Multiply((0.5, 1.5), per_channel=0.5),
-        ])
+            iaa.ChangeColorspace(from_colorspace="HSV", to_colorspace="RGB")]),
+        iaa.Sequential([
+            iaa.ChangeColorspace(from_colorspace="RGB", to_colorspace="HSV"),
+            iaa.WithChannels(1, iaa.Add((50, 100))),
+            iaa.ChangeColorspace(from_colorspace="HSV", to_colorspace="RGB")]),
+        iaa.Sequential([
+            iaa.ChangeColorspace(from_colorspace="RGB", to_colorspace="HSV"),
+            iaa.WithChannels(2, iaa.Add((50, 100))),
+            iaa.ChangeColorspace(from_colorspace="HSV", to_colorspace="RGB")]),
+        iaa.Multiply((0.5, 1.5), per_channel=0.5),
+    ])
 ], random_order=True)

4d7fb19d4833c3b0b424a8de4a236e530b4faa75
Jakub Czakon <jakub.czakon@pascal04.intra.codilime.com>
2018-02-17 22:41:36 +0100

    local

--- utils.py
+++ utils.py
@@ -74,8 +74,7 @@ def read_masks(masks_filepaths):
             mask_dir = mask_dir[0]
         for i, mask_filepath in enumerate(glob.glob('{}/*'.format(mask_dir))):
             blob = np.asarray(Image.open(mask_filepath))
-            blob = blob / blob.max(axis=None).astype(np.float32)
-            blob_binarized = (blob > 0.5).astype(np.uint8) * i
+            blob_binarized = (blob > 128.).astype(np.uint8) * i
             mask.append(blob_binarized)
         mask = np.sum(np.stack(mask, axis=0), axis=0).astype(np.uint8)
         masks.append(mask)

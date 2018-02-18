3916d497f8436df9a4836ddf0076b6483474288e
Jakub Czakon <jakub.czakon@pascal01.intra.codilime.com>
2018-02-18 14:42:47 +0100

    cleaned yaml

--- models.py
+++ models.py
@@ -36,8 +36,8 @@ class PyTorchUNetMultitask(Model):
         self.optimizer = optim.Adam(self.weight_regularization(self.model, **architecture_config['regularizer_params']),
                                     **architecture_config['optimizer_params'])
         self.loss_function = [('mask', segmentation_loss, 0.3),
-                              ('contour', segmentation_loss, 0.4),
-                              ('contour_touching', segmentation_loss, 0.2),
+                              ('contour', segmentation_loss, 0.5),
+                              ('contour_touching', segmentation_loss, 0.1),
                               ('center', segmentation_loss, 0.1)]
         self.callbacks = callbacks_unet(self.callbacks_config)
 

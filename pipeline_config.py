25b66b20dd3aba38dc6eaf3e8002926ec9f92989
kamil-kaczmarek <kamil.kaczmarek@codilime.com>
2018-04-04 21:45:53 +0200

    bug fix

--- pipeline_config.py
+++ pipeline_config.py
@@ -13,7 +13,6 @@ X_COLUMNS = ['file_path_image']
 Y_COLUMNS = ['file_path_mask']
 Y_COLUMNS_MULTITASK = ['file_path_mask',
                        'file_path_contours',
-                       'file_path_contours_touching',
                        'file_path_centers']
 Y_COLUMNS_SCORING = ['file_path_masks']
 

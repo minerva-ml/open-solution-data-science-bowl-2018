1b2ed0c563cc65244e40994a80c2f77bdaa39147
apyskir <andrzej.pyskir@gmail.com>
2018-07-03 14:40:40 +0200

    bring dev_mode to life v2

--- main.py
+++ main.py
@@ -21,46 +21,52 @@ def prepare_masks():
 @action.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.2, required=False)
-def train(pipeline_name, validation_size):
-    pipeline_manager.train(pipeline_name, validation_size)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def train(pipeline_name, validation_size, dev_mode):
+    pipeline_manager.train(pipeline_name, validation_size, dev_mode)
 
 
 @action.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.2, type=str, required=False)
-def evaluate(pipeline_name, validation_size):
-    pipeline_manager.evaluate(pipeline_name, validation_size)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def evaluate(pipeline_name, validation_size, dev_mode):
+    pipeline_manager.evaluate(pipeline_name, validation_size, dev_mode)
 
 
 @action.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
-def predict(pipeline_name):
-    pipeline_manager.predict(pipeline_name)
-
-
-@action.command()
-@click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
-@click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.1, required=False)
-def train_evaluate_predict(pipeline_name, validation_size):
-    pipeline_manager.train(pipeline_name, validation_size)
-    pipeline_manager.evaluate(pipeline_name, validation_size)
-    pipeline_manager.predict(pipeline_name)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def predict(pipeline_name, dev_mode):
+    pipeline_manager.predict(pipeline_name, dev_mode)
 
 
 @action.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.1, required=False)
-def train_evaluate(pipeline_name, validation_size):
-    pipeline_manager.train(pipeline_name, validation_size)
-    pipeline_manager.evaluate(pipeline_name, validation_size)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def train_evaluate_predict(pipeline_name, validation_size, dev_mode):
+    pipeline_manager.train(pipeline_name, validation_size, dev_mode)
+    pipeline_manager.evaluate(pipeline_name, validation_size, dev_mode)
+    pipeline_manager.predict(pipeline_name, dev_mode)
 
 
 @action.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
 @click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.1, required=False)
-def evaluate_predict(pipeline_name, validation_size):
-    pipeline_manager.evaluate(pipeline_name, validation_size)
-    pipeline_manager.predict(pipeline_name)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def train_evaluate(pipeline_name, validation_size, dev_mode):
+    pipeline_manager.train(pipeline_name, validation_size, dev_mode)
+    pipeline_manager.evaluate(pipeline_name, validation_size, dev_mode)
+
+
+@action.command()
+@click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
+@click.option('-v', '--validation_size', help='percentage of training used for validation', default=0.1, required=False)
+@click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
+def evaluate_predict(pipeline_name, validation_size, dev_mode):
+    pipeline_manager.evaluate(pipeline_name, validation_size, dev_mode)
+    pipeline_manager.predict(pipeline_name, dev_mode)
 
 
 if __name__ == "__main__":
